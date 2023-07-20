2bb7ee1a949adf92b95e458628ae79cce4b1f376
roman <roma.anufriev.90@mail.ru>
2023-07-20 20:03:27 +0300

    task16

--- task16.py
+++ task16.py
@@ -8,13 +8,13 @@
 #Решение :
 
 
-list_1 = [1, 2, 3, 4, 5]
-k = 3
-count = 0
-for i in list_1:
-    if i == k:
-        count += 1
-print(f"Число {k} встречается в массиве {count} раз.")
+# list_1 = [1, 2, 3, 4, 5]
+# k = 3
+# count = 0
+# for i in list_1:
+#     if i == k:
+#         count += 1
+# print(f"Число {k} встречается в массиве {count} раз.")
 
 
 
